390e4fbb5465327092d21b8e05cd659f5f3e1c36
Diego Orellana <benderrobot12@gmail.com>
2020-05-21 23:27:03 -0600

    Close register windows when we register the user

--- SistemaGestionMotosUES/FormRegistro.cs
+++ SistemaGestionMotosUES/FormRegistro.cs
@@ -202,6 +202,9 @@ namespace SistemaGestionMotosUES
 
                 vendedor.registrarVendedorDb();
                 MessageBox.Show("Vendedor Registrado");
+
+                    Close();
+                    Dispose();
             }
             else
             {
